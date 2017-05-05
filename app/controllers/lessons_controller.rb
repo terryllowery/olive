535b07d6123ea354d2b3003511bdb5d609b821e4
Terry Lowery <terry@lowerytech.net>
2017-05-05 02:08:32 +0000

    Added lessons check to see if user is enrolled

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :enrolled_in_course
+
   def show
+    if ! enrolled_in_course
+      redirect_to course_path(current_course), alert: "You haven't enrolled yet. Please enroll in course to see details."
+    end
 
   end
 
   private
 
+  def current_course
+    @current_course ||= current_lesson.section.course
+  end
+
+  helper_method :enrolled_in_course
+  def enrolled_in_course
+    @enrolled_in_course ||= current_user.enrolled_in?(current_course)
+  end
 
   helper_method :current_lesson
   def current_lesson
