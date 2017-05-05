20a1a9cb84bb397a4f9e33b59180b85aecbbcc32
Terry Lowery <terry@lowerytech.net>
2017-05-05 03:47:26 +0000

    stripe imp with errors

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'figaro'
 gem 'fog'
 gem 'carrierwave-aws'
 
+# Stripe credit card payments
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use postgresql as the database for Active Record
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,7 +2,27 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
   def create
     current_user.enrollments.create(course: current_course)
+
+
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Rails Stripe customer',
+        currency: 'usd'
+    )
     redirect_to course_path(current_course)
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
   end
 
   private
